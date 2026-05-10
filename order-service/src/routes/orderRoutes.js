4052cd13cecd98cd798f132f72ea4f5b850d628a
menna ebrahim <menna.ebrahim177@gmail.com>
2026-05-10 13:15:05 +0300

    edit-order-service(pick up btn in delivary is worrking)

--- order-service/src/routes/orderRoutes.js
+++ order-service/src/routes/orderRoutes.js
@@ -6,20 +6,25 @@ const {
     getUserOrders, 
     updateOrderStatus, 
     getRestaurantOrders,
-    getAvailableOrders // 👈 1. استدعينا الدالة الجديدة هنا
+    getAvailableOrders,
+    assignDelivery // 👈 1. استدعينا دالة الاستلام هنا
 } = require('../controllers/orderController');
 
 // 1. إنشاء أوردر
 router.post('/', createOrder);
 
-// 2. مسار الدليفري (الجديد) 👈 لازم يكون فوق عشان كلمة available تتقري كمسار مش كـ ID
+// 2. مسارات الدليفري (لازم فوق عشان متتلخبطش مع الـ IDs)
 router.get('/available', getAvailableOrders); 
+
+// 👈 2. المسار الجديد بتاع الاستلام (هنحط POST و PATCH احتياطي عشان الفرونت إند)
+router.post('/assign', assignDelivery); 
+router.patch('/assign', assignDelivery); 
 
 // 3. مسارات الـ GET المحددة
 router.get('/user/:userId', getUserOrders); 
 router.get('/restaurant/:restaurantId', getRestaurantOrders); 
 
-// 4. مسار التحديث
+// 4. مسار التحديث العادي
 router.patch('/:id/status', updateOrderStatus); 
 
 module.exports = router;
